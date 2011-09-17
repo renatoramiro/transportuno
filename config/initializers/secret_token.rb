f779f5210897e8da6ca54b1ffa2f03269b4c0a24
Renato Ramiro <renato.ipj@gmail.com>
2011-09-16 22:21:04 -0300

    Primeiro Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transportuno::Application.config.secret_token = 'a160095ebc9668e1de21f8d63ef72395a336f39f906fac10ccddbfa3142c1d59787097ff2de57c102be80dd2b892817f23c9187d88a0c537cb0cfffd39fd3f62'
